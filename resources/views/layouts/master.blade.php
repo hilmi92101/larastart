de2ab09a48d4d49ae31ae778cb4585e2e761469f
Hilmi <hilmi.azman@srglobal.com>
2020-10-23 22:19:42 +0800

    Logout function and change fontawesome icon color

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -44,6 +44,7 @@
             </a>
             <!-- Sidebar -->
             <div class="sidebar">
+
                <!-- Sidebar user panel (optional) -->
                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                   <div class="image">
@@ -53,22 +54,24 @@
                      <a href="#" class="d-block">{{ Auth::user()->name }}</a>
                   </div>
                </div>
+
                <!-- Sidebar Menu -->
                <nav class="mt-2">
                   <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                     <!-- Add icons to the links using the .nav-icon class
-                        with font-awesome or any other icon font library -->
+                     
                      <li class="nav-item">
                         <router-link to="/dashboard" class="nav-link">
-                           <i class="nav-icon fas fa-tachometer-alt"></i>
+                           <i class="nav-icon fas fa-tachometer-alt blue"></i>
                            <p>
                               Dashboard
                            </p>
                         </router-link>
                      </li>
+
+
                      <li class="nav-item has-treeview">
                         <a href="#" class="nav-link">
-                           <i class="nav-icon fas fa-cog"></i>
+                           <i class="nav-icon fas fa-cog green"></i>
                            <p>
                               Management
                               <i class="right fas fa-angle-left"></i>
@@ -91,20 +94,28 @@
                      </li>
                      <li class="nav-item">
                         <router-link to="/profile" class="nav-link">
-                           <i class="nav-icon fas fa-user"></i>
+                           <i class="nav-icon fas fa-user orange"></i>
                            <p>
                               Profile
                            </p>
                         </router-link>
                      </li>
+
+                     <!-- Logout -->
                      <li class="nav-item">
-                        <a href="#" class="nav-link">
-                           <i class="nav-icon fas fa-power-off"></i>
-                           <p>
-                              Logout
-                           </p>
+
+                        <a class="nav-link" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                           document.getElementById('logout-form').submit();">
+                           <i class="nav-icon fas fa-power-off red"></i>
+                           <p>{{ __('Logout') }}</p>
                         </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                      </li>
+
+
                   </ul>
                </nav>
                <!-- /.sidebar-menu -->
